4c54498e06f53411291dfd50d2f21530ae120a91
vinnya3 <vinaceto@yahoo.com>
2016-07-01 21:50:24 +0000

    added service api to retrieve movies from the server

--- static/js/main.js
+++ static/js/main.js
@@ -43,14 +43,28 @@
 		},
 		render: function() {
 			var movie_el = this.movie_container,
-					movies = this.movies;
+					movies = JSON.parse( this.movies ),
+					movies_length = movies.length;
 			//loop through the movies and grab each movie's info
 			//create an 'movie-card' element, and add a data attribute
 			//container the array index - for use with modal
-			console.log(movies);
+			for(var i = 0; i < movies.length; ++i) {
+				//create a new movie card el
+				var movie_card = document.createElement('div');
+				movie_card_img = document.createElement('img');
+				movie_card_img.className = 'movie-card__image';
+				movie_card_img.src = movies[i]["poster_image"];
+				movie_card.appendChild(movie_card_img);
+				movie_card.setAttribute('data-index', i);
+				movie_card.className = 'movie-card';
+				//append the movie card to the movie container
+				movie_el.appendChild(movie_card);
+			}
 		}
-	}
 
+	} // ./modalView
+
+  //initialize controller
 	controller.init();
 
 })(jQuery);
